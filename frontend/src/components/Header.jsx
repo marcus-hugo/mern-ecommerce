f503e247afbcb9871bd5a5244106c82350e65c2e
marcushugo <phantomboss300@yahoo.com>
2023-06-15 08:21:07 -0500

    set up react router on  the frontend

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,22 +1,30 @@
 import { Navbar, Nav, Container, NavbarBrand } from "react-bootstrap"
 import { FaShoppingCart, FaUser } from "react-icons/fa"
+import { LinkContainer } from "react-router-bootstrap"
+import { Link } from "react-router-dom"
 
 function Header() {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">TechShop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>TechShop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
-                <FaShoppingCart /> Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <FaUser />
-                Login
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser />
+                  Login
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
