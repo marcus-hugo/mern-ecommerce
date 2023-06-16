f4fa06d4301e57951dda76ab5ebde9e80ba07603
marcushugo <phantomboss300@yahoo.com>
2023-06-16 15:51:29 -0500

    updated min-height on main changed footer bg and color changed Rating div to span

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Container, Row, Col } from "react-bootstrap"
 function Footer() {
   const currentYear = new Date().getFullYear()
   return (
-    <footer>
+    <footer className="bg-dark text-light">
       <Container>
         <Row>
           <Col className="text-center py-3">
