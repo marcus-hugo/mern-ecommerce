74d5ecc1b3cf9729753850b97c2a2f5c379fcb24
marcushugo <phantomboss300@yahoo.com>
2023-06-19 10:04:20 -0500

    added custom async handler and error handling

---
+++ backend/middleware/asyncHandler.js
@@ -0,0 +1,6 @@
+// Simple asyc handler function
+const asyncHandler = fn => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next)
+}
+
+export default asyncHandler
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express"
 import dotenv from "dotenv"
 dotenv.config()
-import products from "./data/products.js" // extension required for own modules
 import connectDB from "./config/db.js"
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
+import productRoutes from "./routes/productRoutes.js"
 
 const port = process.env.PORT || 5000
 connectDB()
@@ -12,13 +13,11 @@ app.get("/", (req, res) => {
   res.send("API is running...")
 })
 
-app.get("/api/products", (req, res) => {
-  res.send(products)
-})
+// Api Routes
+app.use("/api/products", productRoutes)
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find(p => p._id === req.params.id)
-  res.json(product)
-})
+// Middleware
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}.`))
