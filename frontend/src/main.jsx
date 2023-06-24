e9974a6a328b3955b02be84afd5fc3d044a025a4
marcushugo <phantomboss300@yahoo.com>
2023-06-24 15:08:12 -0500

    set up redux, store, slices, and constants

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
+import { Provider } from "react-redux"
+import store from "./store"
 import App from "./App.jsx"
 // import "bootstrap/dist/css/bootstrap.min.css"
 import "./assets/index.min.css"
@@ -19,6 +21,8 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 )
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,80 +1,72 @@
-import { useState, useEffect } from "react"
-import axios from "axios"
 import { useParams, Link } from "react-router-dom"
 import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from "react-bootstrap"
 import Rating from "../components/Rating"
+import { useGetProductDetailsQuery } from "../slices/productsApiSlice"
 
 const ProductScreen = () => {
   const { id: productId } = useParams() // get dynamic params from a url
-  const [product, setProduct] = useState({})
-
-  // will run once on load and when product id changes
-  useEffect(() => {
-    const fetchProduct = async () => {
-      try {
-        const { data } = await axios.get(`/api/products/${productId}`)
-        setProduct(data)
-        console.log("product useEffect ran,", `product id: ${productId}`)
-      } catch (e) {
-        console.log(e)
-      }
-    }
-    fetchProduct()
-  }, [productId])
+  const { data: product, isLoading, error } = useGetProductDetailsQuery(productId)
 
   return (
     <>
       <Link to="/" className="btn btn-light my-3">
         Go Back
       </Link>
-      <Row>
-        <Col md={5}>
-          <Card.Img src={product.image} alt={product.name} />
-        </Col>
-        <Col md={4}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h1>{product.name}</h1>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating value={product.rating} text={product.numReviews} />
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Card.Text as="h2">${product.price}</Card.Text>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Card.Text>{product.description}</Card.Text>
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
+
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <div>{error?.data?.error || error.error}</div>
+      ) : (
+        <Row>
+          <Col md={5}>
+            <Card.Img src={product.image} alt={product.name} />
+          </Col>
+          <Col md={4}>
             <ListGroup variant="flush">
               <ListGroup.Item>
-                <Row>
-                  <Col>Price:</Col>
-                  <Col>
-                    <strong>${product.price}</strong>
-                  </Col>
-                </Row>
+                <h1>{product.name}</h1>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Row>
-                  <Col>Status:</Col>
-                  <Col>
-                    <strong>{product.countInStock > 0 ? `${product.countInStock} in stock` : "Out of stock"}</strong>
-                  </Col>
-                </Row>
+                <Rating value={product.rating} text={product.numReviews} />
               </ListGroup.Item>
               <ListGroup.Item>
-                <Button className="btn-block" type="button" disabled={product.countInStock === 0}>
-                  Add to Cart
-                </Button>
+                <Card.Text as="h2">${product.price}</Card.Text>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Card.Text>{product.description}</Card.Text>
               </ListGroup.Item>
             </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      <strong>{product.countInStock > 0 ? `${product.countInStock} in stock` : "Out of stock"}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Button className="btn-block" type="button" disabled={product.countInStock === 0}>
+                    Add to Cart
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </>
   )
 }
