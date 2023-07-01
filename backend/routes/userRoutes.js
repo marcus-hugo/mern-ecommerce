3c7c8d5118eb0c5eb677753024e4027180e6c78c
marcushugo <phantomboss300@yahoo.com>
2023-07-01 11:31:40 -0500

    created user controller and routes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,13 @@
+import express from "express"
+import { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile, getUsers, getUserById, deleteUser, updateUser } from "../controllers/userController.js"
+
+const router = express.Router()
+
+//start here - all connected to "/api/users"
+router.route("/").post(registerUser).get(getUsers)
+router.post("/logout", logoutUser)
+router.post("/login", authUser)
+router.route("/profile").get(getUserProfile).put(updateUserProfile)
+router.route("/:id").delete(deleteUser).get(getUserById).put(updateUser)
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ dotenv.config()
 import connectDB from "./config/db.js"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 import productRoutes from "./routes/productRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
 
 const port = process.env.PORT || 5000
 connectDB()
@@ -15,6 +16,9 @@ app.get("/", (req, res) => {
 
 // Api Routes
 app.use("/api/products", productRoutes)
+
+// User Routes
+app.use("/api/users", userRoutes)
 
 // Middleware
 app.use(notFound)
