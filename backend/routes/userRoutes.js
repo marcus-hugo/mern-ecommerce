bc8ab66bc0cd3c988241057590cf3a304893f385
marcushugo <phantomboss300@yahoo.com>
2023-07-02 13:29:59 -0500

    set up jwt in http only cookie for auth

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,14 @@
 import express from "express"
 import { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile, getUsers, getUserById, deleteUser, updateUser } from "../controllers/userController.js"
+import { protect, admin } from "../middleware/authMiddleware.js"
 
 const router = express.Router()
 
 //start here - all connected to "/api/users"
-router.route("/").post(registerUser).get(getUsers)
+router.route("/").post(registerUser).get(protect, admin, getUsers)
 router.post("/logout", logoutUser)
-router.post("/login", authUser)
-router.route("/profile").get(getUserProfile).put(updateUserProfile)
-router.route("/:id").delete(deleteUser).get(getUserById).put(updateUser)
+router.post("/auth", authUser)
+router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile)
+router.route("/:id").delete(protect, admin, deleteUser).get(protect, admin, getUserById).put(protect, admin, updateUser)
 
 export default router
